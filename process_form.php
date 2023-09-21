1dc95defc98b3ed8d16be01d78a3d3a7153530cd
Alex_D <shram.monolit@mail.ru>
2023-09-22 01:21:38 +0300

    Made table inside mail

--- process_form.php
+++ process_form.php
@@ -1,4 +1,13 @@
 <?php
+function generateFeedbackTable($name, $surname, $email, $phone, $text) {
+    $table = '<table style="border-collapse: collapse; width: 100%; max-width: 500px;">';
+    $table .= '<tr><td style="padding: 8px; border: 1px solid #ccc;">Фамилия Имя:</td><td style="padding: 8px; border: 1px solid #ccc;">' . $name . ' ' . $surname . '</td></tr>';
+    $table .= '<tr><td style="padding: 8px; border: 1px solid #ccc;">Email:</td><td style="padding: 8px; border: 1px solid #ccc;">' . $email . '</td></tr>';
+    $table .= '<tr><td style="padding: 8px; border: 1px solid #ccc;">Номер телефона:</td><td style="padding: 8px; border: 1px solid #ccc;">' . $phone . '</td></tr>';
+    $table .= '<tr><td style="padding: 8px; border: 1px solid #ccc;">Текст:</td><td style="padding: 8px; border: 1px solid #ccc;">' . $text . '</td></tr>';
+    $table .= '</table>';
+    return $table;
+}
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $properties_file = 'application.properties';
@@ -17,20 +26,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $phone = clear_data($_POST['phone']);
     $text = clear_data($_POST['message']);
 
-    $message = 'Фамилия Имя: ' . $name." " .  $surname."\n" . 'Email: ' . $email."\n" . 'Номер телефона: ' . $phone."\n" . 'Текст: ' . $text;
+    $message = '<html><body>';
+    $message .= '<h1>Данные формы обратной связи:</h1>';
+    $message .= generateFeedbackTable($name, $surname, $email, $phone, $text);
+    $message .= '</body></html>';
 
-    $to = "shram.monolit@mail.ru";
+    $to = $properties['to_email'];
     $subject = "Feedback";
 
-    $headers = "From: site@example.com\r\n";
-    $headers .= "Reply-To: site@example.com\r\n";
-    $headers .= "Content-Type: text/plain; charset=utf-8\r\n";
+    $headers = "From: $email \r\n";
+    $headers .= "Reply-To: $email \r\n";
+    $headers .= "Content-Type: text/html; charset=utf-8\r\n";
+
 
     if (mail($to, $subject, $message, $headers)) {
-        echo '<script>alert("Письмо успешно отправлено.");</script>';
-      } else {
-        echo '<script>alert("Ошибка при отправке письма.");</script>';
-      }
+        echo '<script>
+            alert("Письмо успешно отправлено.");
+            window.location.href = "index.html"; // Переход на index.html после закрытия alert
+        </script>';
+    } else {
+        echo '<script>
+            alert("Ошибка при отправке письма.");
+            window.location.href = "index.html"; // Переход на index.html после закрытия alert
+        </script>';
+    }
 } else {
     echo '<script>alert("Ошибка сервера.");</script>';
 }
