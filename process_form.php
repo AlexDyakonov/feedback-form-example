df5c017d9f3eda9bf364711b566ef39d0650d981
Alex_D <shram.monolit@mail.ru>
2023-09-21 23:28:45 +0300

    Now form sending messages

--- process_form.php
+++ process_form.php
@@ -1,24 +1,37 @@
 <?php
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $properties_file = 'application.properties';
+    $properties = parse_ini_file($properties_file);
 
-    $name = $_POST["validationServer01"];
-    $surname = $_POST["validationServer02"];
-    $email = $_POST["exampleFormControlInput1"];
-    $phone = $_POST["phone-input"];
-    $message = $_POST["exampleFormControlTextarea1"];
-
-    $to = $properties['to_email'];
-
-    $subject = "Сообщение с веб-формы";
-
-    $message_body = "Имя: $name\nФамилия: $surname\nEmail: $email\nНомер телефона: $phone\nСообщение:\n$message";
-
-    if (mail($to, $subject, $message_body)) {
-        echo "Сообщение успешно отправлено.";
-    } else {
-        echo "Произошла ошибка при отправке сообщения.";
+    function clear_data($val){
+        $val = trim($val);
+        $val = stripcslashes($val);
+        $val = htmlspecialchars($val);
+        return $val;
     }
+
+    $name = clear_data($_POST['name']);
+    $surname = clear_data($_POST['surname']);
+    $email = clear_data($_POST['email']);
+    $phone = clear_data($_POST['phone']);
+    $text = clear_data($_POST['message']);
+
+    $message = 'Фамилия Имя: ' . $name." " .  $surname."\n" . 'Email: ' . $email."\n" . 'Номер телефона: ' . $phone."\n" . 'Текст: ' . $text;
+
+    $to = "shram.monolit@mail.ru";
+    $subject = "Feedback";
+
+    $headers = "From: site@example.com\r\n";
+    $headers .= "Reply-To: site@example.com\r\n";
+    $headers .= "Content-Type: text/plain; charset=utf-8\r\n";
+
+    if (mail($to, $subject, $message, $headers)) {
+        echo '<script>alert("Письмо успешно отправлено.");</script>';
+      } else {
+        echo '<script>alert("Ошибка при отправке письма.");</script>';
+      }
 } else {
-    echo "Доступ запрещен.";
+    echo '<script>alert("Ошибка сервера.");</script>';
 }
 ?>
